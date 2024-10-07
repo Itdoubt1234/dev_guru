65706fc192bc055ce1efa8604ab58de0015ee798
guruvachan-01 <156888776+guruvachan-01@users.noreply.github.com>
2024-10-07 21:04:45 +0530

    edit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,49 +1,52 @@
 import React from "react";
+import img1 from "../assets/img/blog1.jpg";
+import img2 from "../assets/img/blog2.jpg";
+import img3 from "../assets/img/blog3.jpg";
 
 const services = [
   {
     title: "Web Development",
     description:
       "We provide modern and responsive web development services with the latest technologies.",
-    icon: "💻",
+    imgUrl: img1,
   },
   {
     title: "UI/UX Design",
     description:
       "Crafting user-friendly and beautiful interfaces for web and mobile applications.",
-    icon: "🎨",
+    imgUrl: img2,
   },
   {
     title: "Backend Developer",
     description:
       "Boost your online presence with our expert SEO services and rank higher on search engines.",
-    icon: "📈",
+    imgUrl: img3,
   },
   {
     title: "React Developer",
     description:
       "Boost your online presence with our expert SEO services and rank higher on search engines.",
-    icon: "📑",
+    imgUrl: img1,
   },
   {
     title: "Java Full stack",
     description:
       "Boost your online presence with our expert SEO services and rank higher on search engines.",
-    icon: "🧑‍💻",
+    imgUrl: img2,
   },
   {
     title: "SEO Optimization",
     description:
       "Boost your online presence with our expert SEO services and rank higher on search engines.",
-    icon: "📈",
+    imgUrl: img3,
   },
 ];
 
 const Services = () => {
   return (
-    <div className="bg-gray-50 py-12">
+    <div className="bg-white py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
-        <div className="text-center ">
+        <div className="text-center">
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-blue-400 sm:text-4xl">
             Our Services
           </p>
@@ -51,25 +54,28 @@ const Services = () => {
             From development to design, we have all the Technologies you need.
           </p>
         </div>
-        <div className="mt-10 ">
-          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="mt-32 bg-opacity-25">
+          <div className="grid grid-cols-1 gap-14 sm:grid-cols-2 lg:grid-cols-3">
             {services.map((service, index) => (
               <div
                 key={index}
                 className="relative group p-6 bg-white rounded-lg shadow-lg transition-transform transform hover:-translate-y-2 hover:shadow-2xl duration-300"
               >
-                <div className="flex items-center justify-center w-16 h-16 bg-indigo-100 text-indigo-500 rounded-full mx-auto group-hover:bg-blue-300 transition-colors duration-300">
-                  <span className="text-3xl">{service.icon}</span>
+                <div className="flex items-center justify-center w-52 h-16 text-blue-500 rounded-full mx-auto  ">
+                  <img
+                    src={service.imgUrl}
+                    alt={service.title}
+                    className="w-full h-26 object-cover group-hover:scale-150 transition-transform duration-300 rounded-lg"
+                  />
                 </div>
-                <h3 className="mt-8 text-lg font-medium text-gray-900 text-center">
+                <h3 className="mt-10 text-lg font-bold text-blue-800 text-center">
                   {service.title}
                 </h3>
-                <p className="mt-4 text-base text-gray-500 text-center">
+                <p className="mt-4 text-base text-gray-500 hover:text-blue-800 text-center">
                   {service.description}
                 </p>
 
-                {/* Add subtle animation on hover */}
-                <div className="absolute inset-0 rounded-lg bg-indigo-500 opacity-0 group-hover:opacity-30 transition-opacity duration-300"></div>
+                <div className="absolute inset-0 rounded-lg transition-opacity duration-300"></div>
               </div>
             ))}
           </div>
