12a0b728bd178628c79e78c2ff239edd5bbc933f
guruvachan-01 <156888776+guruvachan-01@users.noreply.github.com>
2024-10-07 21:07:25 +0530

    edit

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -31,7 +31,7 @@ const blogPosts = [
 
 const Blog = () => {
   return (
-    <div className="bg-gray-50 py-12">
+    <div className="bg-white py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center">
           <h2 className="text-3xl font-extrabold text-blue-400">
