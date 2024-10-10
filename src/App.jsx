06e6b8ac1833472fa701834e4e33ee87a12d4df2
guruvachan-01 <156888776+guruvachan-01@users.noreply.github.com>
2024-10-10 10:38:17 +0530

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import React from "react";
 import About from "./anothercomponents/About";
 import Technology from "./anothercomponents/Technology";
 import Footer from "./anothercomponents/Footer";
+import Landing from "./anothercomponents/Landing";
+import Future from "./anothercomponents/Future";
+
+import About1 from "./anothercomponents/About1";
+import Features from "./anothercomponents/Features";
+import Navbar from "./anothercomponents/Navbar";
 
 function App() {
   return (
     <>
       <div className="bg-slate-100">
+        <Navbar />
+        <About1 />
         <About />
         <Technology />
         <Footer />
+        <Landing />
+        <Features />
+        <Future />
       </div>
     </>
   );
